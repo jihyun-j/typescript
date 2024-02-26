eb8d47b10dd4b5d4b8ced764fde4dc1a099208b1
jihyun-j <jihyun9142@gmail.com>
2024-02-26 13:24:23 +0900

    수동으로 자바스크립트 타입지정

--- coreTypes1/app.ts
+++ coreTypes1/app.ts
@@ -17,5 +17,19 @@ function add1(n1: number, n2: number) {
 const number3 = "5";
 const number4 = 2.8;
 
-const result1 = add1(number3, number4);
-console.log(result);
+const result1 = add1(number3, number4); // number가 아닌 다른 타입을 넣으면 에러발생 (개발중에 에러발견 가능)
+console.log(result1);
+
+// 3. 자바스크립트에서 타입 지정
+function add3(n1: number, n2: number) {
+  if (typeof n1 === "number" && typeof n2 === "number") {
+    throw new Error("Incorrect input");
+  }
+  return n1 + n2;
+}
+
+const number5 = "5";
+const number6 = 2.8;
+
+const result2 = add1(number5, number6);
+console.log(result2);
