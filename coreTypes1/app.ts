fa14fabf0c7e60c6c6cf8388f4999c67481f3fd7
jihyun-j <jihyun9142@gmail.com>
2024-02-26 13:16:35 +0900

    타입스크립트 function

--- coreTypes1/app.ts
+++ coreTypes1/app.ts
@@ -8,3 +8,14 @@ const number2 = 2.8;
 
 const result = add(number1, number2);
 console.log(result);
+
+// 2. 타입 추가
+function add1(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number3 = "5";
+const number4 = 2.8;
+
+const result1 = add1(number3, number4);
+console.log(result);
