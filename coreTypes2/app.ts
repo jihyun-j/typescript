7c50d46aeda349db245aa8a825c2d1bb5785e5b2
jihyun-j <jihyun9142@gmail.com>
2024-02-26 14:43:06 +0900

    object type

---
+++ coreTypes2/app.ts
@@ -0,0 +1,11 @@
+// object type: key-type pairs
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Maximilian",
+  age: 30,
+};
+
+console.log(person.name);
