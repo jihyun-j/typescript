6e77a36be4397215ee0d841f6cd76f4ee3abfc22
jihyun-j <jihyun9142@gmail.com>
2024-02-28 16:07:45 +0900

    unknown type file 추가

--- unknown/app.js
+++ unknown/app.js
@@ -1,19 +1,15 @@
-let userInput: unknown;
-let userName: string;
-
+var userInput;
+var userName;
 userInput = 5;
 userName = "Max";
 // userName = userInput; // Error: unknown type은 string에 할당되지 않음
 // 하지만 any타입은 가장 유연한 타입이기 때문에 모든 타입을 허용함
-
 // 만약 unknown을 사용하면서 string을 할당하려면 추가 조건이 필요
 if (typeof userInput === "string") {
-  userName = userInput;
+    userName = userInput;
 }
-
 // naver type
-function generateError(message: string, code: number) {
-  throw { message: message, errorCode: code };
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-
 generateError("An error ", 500);
