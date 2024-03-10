53f90e2769c205a5234dbe6b509c59d55f36c354
jihyun-j <jihyun9142@gmail.com>
2024-03-10 11:00:43 +0900

    intersection type

---
+++ intersectionType/app.ts
@@ -0,0 +1,24 @@
+// intersection type(교차유형)은 다른 유형을 결합하게 해줌
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
