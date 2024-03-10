5a945cdb1e7a9787fe06e90e9b21cfbd3e8fbc50
jihyun-j <jihyun9142@gmail.com>
2024-03-10 11:21:31 +0900

    type guards

--- intersectionType/app.js
+++ intersectionType/app.js
@@ -4,3 +4,52 @@ var e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+// Type Guards
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    // 커스텀 타입 대신에 'in' 사용 (타입스크립트 빌트인)
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Name: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1); // Name: Max, Name: create-server, Name: Sun Mar 10 2024 11:11:19 GMT+0900 (Korean Standard Time)
+printEmployeeInformation({ name: "Jihyun", startDate: new Date() }); // Name: Jihyun Name: Sun Mar 10 2024 11:11:19 GMT+0900 (Korean Standard Time)
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving...");
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving a truck...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Loading cargo" + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // vehicle instanceof Truck
+    if ("loadCargo" in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- intersectionType/app.ts
+++ intersectionType/app.ts
@@ -22,3 +22,61 @@ const e1: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+// Type Guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  // 커스텀 타입 대신에 'in' 사용 (타입스크립트 빌트인)
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Name: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1); // Name: Max, Name: create-server, Name: Sun Mar 10 2024 11:11:19 GMT+0900 (Korean Standard Time)
+printEmployeeInformation({ name: "Jihyun", startDate: new Date() }); // Name: Jihyun Name: Sun Mar 10 2024 11:11:19 GMT+0900 (Korean Standard Time)
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo" + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // vehicle instanceof Truck
+  if ("loadCargo" in vehicle) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
